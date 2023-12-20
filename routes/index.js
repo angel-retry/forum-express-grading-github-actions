748250760b19770e41e08f70db32810e7e3bfcc1
a27657060 <a27657060@gmail.com>
2023-12-20 15:05:50 +0800

    feat: add admin index page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const restController = require('../controllers/restaurant-controllers')
+const admin = require('./modules/admin')
 
+router.use('/admin', admin)
 router.get('/restaurants', restController.getRestaurants)
 
 // 當都沒有匹對的路徑，最後會導向此路由
