74bd2e2c72ceb64850377a85beb4b413c6b94f0f
a27657060 <a27657060@gmail.com>
2023-12-20 15:31:35 +0800

    user signup

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const restController = require('../controllers/restaurant-controllers')
+const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
+const userController = require('../controllers/user-controllers')
 
 router.use('/admin', admin)
 router.get('/restaurants', restController.getRestaurants)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 
 // 當都沒有匹對的路徑，最後會導向此路由
 router.use('/', (req, res) => res.redirect('/restaurants'))
