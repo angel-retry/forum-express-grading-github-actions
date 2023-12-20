1636d63f8f8199af99060e7f029000445573687b
a27657060 <a27657060@gmail.com>
2023-12-20 14:52:30 +0800

    feat: add index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const restController = require('../controllers/restaurant-controllers')
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+router.get('/restaurants', restController.getRestaurants)
+
+// 當都沒有匹對的路徑，最後會導向此路由
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
