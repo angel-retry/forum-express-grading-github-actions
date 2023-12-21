cda48bc9efef559b4549dfcb4f3cf02c4fe64d3d
a27657060 <a27657060@gmail.com>
2023-12-21 11:20:25 +0800

    feat: create restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -9,6 +9,25 @@ const adminController = {
         return res.render('admin/restaurants', { restaurants })
       })
       .catch(err => next(err))
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('請輸入餐廳名字!')
+    Restaurant.create({
+      name,
+      tel,
+      address,
+      openingHours,
+      description
+    })
+      .then(() => {
+        req.flash('success_messages', '新增成功!')
+        res.redirect('/admin/restaurants')
+      })
+      .catch(err => next(err))
   }
 
 }
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
+router.get('/restaurants/create', adminController.createRestaurant)
+router.post('/restaurants', adminController.postRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
