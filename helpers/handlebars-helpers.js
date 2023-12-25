defa4457ea1e8cf87372699360fa3bc51292e4d9
a27657060 <a27657060@gmail.com>
2023-12-25 10:19:30 +0800

    feat: add ifCond hbs helper & update categories selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,5 +1,9 @@
 const dayjs = require('dayjs')
 
 module.exports = {
-  currentYear: () => dayjs().year() // 取得當年年份作為 currentYear 的屬性值，並導出
+  currentYear: () => dayjs().year(), // 取得當年年份作為 currentYear 的屬性值，並導出
+  // 接收兩個參數，如果a=b就回傳{{#ifCond}}{{/ifCond}}內容
+  ifCond: function (a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this)
+  }
 }
