fa27cf4849e69fd1e45ef0ae4aa9820dc271ed56
a27657060 <a27657060@gmail.com>
2024-03-22 11:36:59 +0800

    feat: adjust middleware/auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,15 @@
-const { ensureAuthenticated, getUser } = require('../helpers/auth-helpers')
+const helpers = require('../helpers/auth-helpers')
 
 // 一般user確認
 const authenticated = (req, res, next) => {
-  if (ensureAuthenticated(req)) return next()
+  if (helpers.ensureAuthenticated(req)) return next()
   return res.redirect('/signin')
 }
 
 // admin使用者確認
 const authenticatedAdmin = (req, res, next) => {
-  if (ensureAuthenticated(req)) {
-    if (getUser(req).isAdmin) {
+  if (helpers.ensureAuthenticated(req)) {
+    if (helpers.getUser(req).isAdmin) {
       return next()
     }
     return res.redirect('/')
